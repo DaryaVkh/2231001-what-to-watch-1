517530779369d9d27295c073e7ce9240d5a91380
Darya <dashav1605@mail.ru>
2022-11-15 01:34:57 +0500

    Больше подробностей (часть 2)

--- project/src/pages/film/film-page.tsx
+++ project/src/pages/film/film-page.tsx
@@ -12,94 +12,97 @@ type Props = {
 };
 
 const FilmPage: FC<Props> = (props) => {
-  const { films } = props;
-  const { filmId } = useParams();
+  const {films} = props;
+  const {filmId} = useParams();
   const film = getFilmById(Number(filmId));
 
-  return film
-    ? (
-      <>
-        <section className="film-card film-card--full">
-          <div className="film-card__hero">
-            <div className="film-card__bg">
-              <img src={film.posterImage} alt={film.name}/>
-            </div>
+  if (!film) {
+    return <Error404Page/>;
+  }
 
-            <h1 className="visually-hidden">WTW</h1>
-
-            <header className="page-header film-card__head">
-              <Logo/>
-
-              <ul className="user-block">
-                <li className="user-block__item">
-                  <div className="user-block__avatar">
-                    <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-                  </div>
-                </li>
-                <li className="user-block__item">
-                  <a href={'/'} className="user-block__link">Sign out</a>
-                </li>
-              </ul>
-            </header>
-
-            <div className="film-card__wrap">
-              <div className="film-card__desc">
-                <h2 className="film-card__title">{film.name}</h2>
-                <p className="film-card__meta">
-                  <span className="film-card__genre">{film.genre}</span>
-                  <span className="film-card__year">{film.released}</span>
-                </p>
-
-                <div className="film-card__buttons">
-                  <button className="btn btn--play film-card__button" type="button">
-                    <svg viewBox="0 0 19 19" width="19" height="19">
-                      <use xlinkHref="#play-s"/>
-                    </svg>
-                    <span>Play</span>
-                  </button>
-                  <button className="btn btn--list film-card__button" type="button">
-                    <svg viewBox="0 0 19 20" width="19" height="20">
-                      <use xlinkHref="#add"/>
-                    </svg>
-                    <span>My list</span>
-                    <span className="film-card__count">9</span>
-                  </button>
-                  <Link to={`/films/${film.id}/review`} className="btn film-card__button">Add review</Link>
-                </div>
-              </div>
-            </div>
+  return (
+    <>
+      <section className="film-card film-card--full">
+        <div className="film-card__hero">
+          <div className="film-card__bg">
+            <img src={film.posterImage} alt={film.name}/>
           </div>
 
-          <div className="film-card__wrap film-card__translate-top">
-            <div className="film-card__info">
-              <div className="film-card__poster film-card__poster--big">
-                <img src={film.posterImage} alt={film.name} width="218" height="327"/>
-              </div>
+          <h1 className="visually-hidden">WTW</h1>
 
-              <div className="film-card__desc">
-                <Tabs film={film}/>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <div className="page-content">
-          <section className="catalog catalog--like-this">
-            <h2 className="catalog__title">More like this</h2>
-            <FilmList films={films.filter((otherFilm) => otherFilm !== film && otherFilm.genre === film.genre).slice(0, 4)} />
-          </section>
-
-          <footer className="page-footer">
+          <header className="page-header film-card__head">
             <Logo/>
 
-            <div className="copyright">
-              <p>© 2019 What to watch Ltd.</p>
+            <ul className="user-block">
+              <li className="user-block__item">
+                <div className="user-block__avatar">
+                  <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+                </div>
+              </li>
+              <li className="user-block__item">
+                <a href={'/'} className="user-block__link">Sign out</a>
+              </li>
+            </ul>
+          </header>
+
+          <div className="film-card__wrap">
+            <div className="film-card__desc">
+              <h2 className="film-card__title">{film.name}</h2>
+              <p className="film-card__meta">
+                <span className="film-card__genre">{film.genre}</span>
+                <span className="film-card__year">{film.released}</span>
+              </p>
+
+              <div className="film-card__buttons">
+                <button className="btn btn--play film-card__button" type="button">
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"/>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list film-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"/>
+                  </svg>
+                  <span>My list</span>
+                  <span className="film-card__count">9</span>
+                </button>
+                <Link to={`/films/${film.id}/review`} className="btn film-card__button">Add review</Link>
+              </div>
             </div>
-          </footer>
+          </div>
         </div>
-      </>
-    )
-    : <Error404Page/>;
+
+        <div className="film-card__wrap film-card__translate-top">
+          <div className="film-card__info">
+            <div className="film-card__poster film-card__poster--big">
+              <img src={film.posterImage} alt={film.name} width="218" height="327"/>
+            </div>
+
+            <div className="film-card__desc">
+              <Tabs film={film}/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+          <FilmList
+            films={films.filter((otherFilm) => otherFilm !== film && otherFilm.genre === film.genre).slice(0, 4)}/>
+        </section>
+
+        <footer className="page-footer">
+          <Logo/>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 };
 
 export default FilmPage;
