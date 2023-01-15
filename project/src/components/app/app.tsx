8498a540151ef689032f1f0497fa9dddaa833e10
Darya V <dashav1605@mail.ru>
2023-01-15 15:14:43 +0400

    Оптимизируй это (Часть 2)

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import browserHistory from '../../browser-history';
 import { AppRoute } from '../../common/models';
-import { useAppSelector } from '../../hooks/store-helpers';
 import AddReviewPage from '../../pages/add-review/add-review-page';
 import Error404Page from '../../pages/error-404/error-404-page';
 import FilmPage from '../../pages/film/film-page';
@@ -10,32 +9,27 @@ import MainPage from '../../pages/main/main-page';
 import MyListPage from '../../pages/my-list/my-list-page';
 import PlayerPage from '../../pages/player/player-page';
 import SignInPage from '../../pages/sign-in/sign-in-page';
-import { getFilms } from '../../store/app/app-selectors';
 import HistoryRouter from '../history-route/history-route';
 import PrivateRoute from '../private-route/private-route';
 import ScrollToTop from '../scroll-to-top/scroll-to-top';
 
-const App: FC = () => {
-  const films = useAppSelector(getFilms);
-
-  return (
-    <HistoryRouter history={browserHistory}>
-      <ScrollToTop />
-      <Routes>
-        <Route path={AppRoute.Main}>
-          <Route index element={<MainPage />}/>
-          <Route path={AppRoute.SignIn} element={<SignInPage />}/>
-          <Route path={AppRoute.MyList} element={<PrivateRoute><MyListPage/></PrivateRoute>}/>
-          <Route path={AppRoute.Film}>
-            <Route index element={<FilmPage />}/>
-            <Route path={AppRoute.AddReview} element={<AddReviewPage />}/>
-          </Route>
-          <Route path={AppRoute.Player} element={<PlayerPage />}/>
-          <Route path={AppRoute.Error404} element={<Error404Page />}/>
+const App: FC = () => (
+  <HistoryRouter history={browserHistory}>
+    <ScrollToTop/>
+    <Routes>
+      <Route path={AppRoute.Main}>
+        <Route index element={<MainPage/>}/>
+        <Route path={AppRoute.SignIn} element={<SignInPage/>}/>
+        <Route path={AppRoute.MyList} element={<PrivateRoute><MyListPage/></PrivateRoute>}/>
+        <Route path={AppRoute.Film}>
+          <Route index element={<FilmPage/>}/>
+          <Route path={AppRoute.AddReview} element={<AddReviewPage/>}/>
         </Route>
-      </Routes>
-    </HistoryRouter>
-  );
-};
+        <Route path={AppRoute.Player} element={<PlayerPage/>}/>
+        <Route path={AppRoute.Error404} element={<Error404Page/>}/>
+      </Route>
+    </Routes>
+  </HistoryRouter>
+);
 
 export default App;
